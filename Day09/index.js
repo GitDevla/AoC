833fea99af90bb87eb5dc43cd8f84e914da3d3d6
GitDevla <pataki.david.ferenc@gmail.com>
2022-12-10 16:06:28 +0100

    Commited wrong code

--- Day09/index.js
+++ Day09/index.js
@@ -8,54 +8,40 @@ class Cord {
 }
 
 class Rope {
-    Head = new Cord(0, 0);
-    Tails = [];
-
-    constructor(length = 1) {
-        for (let i = 0; i < length; i++) {
-            this.Tails.push(new Cord(0, 0));
-        }
+    Knots = [];
+    constructor(length) {
+        for (let i = 0; i < length; i++)
+            this.Knots.push(new Cord(0, 0));
+        this.Head = this.Knots[0];
     }
 
     moveUp() {
         this.Head.y++;
-        this.tailLogic(0, 1);
+        this.tailLogic(this.Head);
     }
     moveDown() {
         this.Head.y--;
-        this.tailLogic(0, -1);
+        this.tailLogic(this.Head);
     }
     moveRigth() {
         this.Head.x++;
-        this.tailLogic(1, 0);
+        this.tailLogic(this.Head);
     }
     moveLeft() {
         this.Head.x--;
-        this.tailLogic(-1, 0);
+        this.tailLogic(this.Head);
     }
 
-
-    tailLogic(x = 0, y = 0, before = this.Head, currI = 0) {
-        let curr = this.Tails[currI];
+    tailLogic(before, currI = 1) {
+        let curr = this.Knots[currI];
         if (curr == undefined) return;
         let diff = Math.max(Math.abs(before.y - curr.y), Math.abs(before.x - curr.x));
         if (diff < 2) return;
-        curr.x += x;
-        curr.y += y;
-        let rowDiff = 0;
-        let colDiff = 0;
-        if (Math.abs(x) == Math.abs(y)) {
-
-        }
-        else if (x != 0) {
-            rowDiff = before.y - curr.y;
-            curr.y += rowDiff;
-        }
-        else if (y != 0) {
-            colDiff = before.x - curr.x;
-            curr.x += colDiff;
-        }
-        this.tailLogic(x + colDiff, y + rowDiff, curr, currI + 1);
+        let dirX = before.x - curr.x;
+        let dirY = before.y - curr.y;
+        curr.x += Math.abs(dirX) === 2 ? dirX / 2 : dirX;
+        curr.y += Math.abs(dirY) === 2 ? dirY / 2 : dirY;
+        this.tailLogic(curr, currI + 1);
     }
 }
 
@@ -84,7 +70,7 @@ function Task(moves, length) {
                     rope.moveRigth();
                     break;
             }
-            let last = rope.Tails.at(-1);
+            let last = rope.Knots.at(-1);
             tiles.add(`${last.y},${last.x} `);
         }
     }
@@ -95,7 +81,7 @@ function PartTwo(moves) {
     return Task(moves, 10);
 }
 function PartOne(moves) {
-    return Task(moves, 1);
+    return Task(moves, 2);
 }
 
 // TESTS
