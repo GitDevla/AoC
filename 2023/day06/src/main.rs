bd9bd19eab929a70fd89bb3a9a86347f7ca018f9
GitDevla <pataki.david.ferenc@gmail.com>
2023-12-06 13:21:34 +0100

    23.06 mathematical solution

--- 2023/day06/src/main.rs
+++ 2023/day06/src/main.rs
@@ -11,13 +11,13 @@ fn pt1() {
     let test_input = r#"Time:      7  15   30
 Distance:  9  40  200"#;
     let test_input = parse(test_input.lines().map(String::from).collect());
-    assert_eq!(task1(test_input), 288);
+    assert_eq!(solve(test_input), 288);
 
     // Solution
     let input: Vec<String> = common::read_file(INPUT_FILE);
     let input = parse(input);
 
-    let ans = task1(input);
+    let ans = solve(input);
     println!("Task 1 solution: {ans}");
 }
 
@@ -27,18 +27,18 @@ fn pt2() {
 Distance:  9  40  200"#;
     let test_input = test_input.replace(" ", "");
     let test_input = parse(test_input.lines().map(String::from).collect());
-    assert_eq!(task1(test_input), 71503);
+    assert_eq!(solve(test_input), 71503);
 
     // Solution
     let input: Vec<String> = common::read_file(INPUT_FILE);
     let input = input.iter().map(|f| f.replace(" ", "")).collect();
     let input = parse(input);
 
-    let ans = task1(input);
+    let ans = solve(input);
     println!("Task 2 solution: {ans}");
 }
 
-fn task1(input: Vec<Race>) -> i32 {
+fn solve(input: Vec<Race>) -> i32 {
     input
         .iter()
         .map(|i| count_possible_wins(*i))
@@ -47,18 +47,13 @@ fn task1(input: Vec<Race>) -> i32 {
 }
 
 fn count_possible_wins(input: Race) -> i32 {
-    let mut wins = 0;
-    for i in 1..input.time {
-        let potential_d = (input.time - i) * i;
-        if input.distance < potential_d {
-            wins += 1;
-        }
-    }
-    return wins;
-}
-
-fn task2(input: String) -> i32 {
-    0
+    let a = -1.0;
+    let b = input.time as f64;
+    let c = -(input.distance as f64);
+    let d = b.powi(2) - 4.0 * a * c;
+    let x1 = (-b + d.sqrt()) / (2.0 * a) + 0.001;
+    let x2 = (-b - d.sqrt()) / (2.0 * a) - 0.001;
+    return x2.floor() as i32 - x1.ceil() as i32 + 1;
 }
 
 fn parse(input: Vec<String>) -> Vec<Race> {
