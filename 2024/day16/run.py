87ca9b8ae29ecbc424237dab7591084d54d4c1f4
GitDevla <pataki.david.ferenc@gmail.com>
2024-12-16 17:57:52 +0100

    24.16/2 slow

--- 2024/day16/run.py
+++ 2024/day16/run.py
@@ -75,10 +75,30 @@ def pt2():
 ###############"""
     test_input = parse(read_test(test_input))
     test(task2(test_input), 45)
+    test_input = """#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################"""
+    test_input = parse(read_test(test_input))
+    test(task2(test_input), 64)
 
-    # Solution
-    # input = read_file(FILE)
-    # print(f"Task 2 solution: {task2(input)}")
+    # # Solution
+    input = read_file(FILE)
+    input = parse(input)
+    print(f"Task 2 solution: {task2(input)}")
 
 
 #########################################################
@@ -127,10 +147,59 @@ def task1(input):
     end = findEnd(input)
     return findPath(input,player,end)
 
+import math
+from collections import defaultdict
+def findPathSquares(map, playerPos, end):
+    player = (0,playerPos,(1,0),(None,None,None,None))
+    queue = PriorityQueue()
+    backtrack = defaultdict(set)
+    queue.put(player)
+    seen = {}
+    limit = math.inf
+    while not queue.empty():
+        score,pos,dir,prev = queue.get()
+        if score > limit:
+            continue
+        px,py,pdx,pdy = prev
+        if seen.get((pos,dir),math.inf) < score: continue
+        if seen.get((pos,dir),score) != score:
+            backtrack[pos,dir].clear()
+        seen[pos,dir] = score
 
+        backtrack[pos,dir].add(((px,py),(pdx,pdy)))
+
+        if pos == end:
+            limit = score
+            continue
+        lookahead = (pos[0]+dir[0],pos[1]+dir[1])
+        if map[lookahead[1]][lookahead[0]] != "#":
+            queue.put((score+1,lookahead,dir,(pos[0],pos[1],dir[0],dir[1])))
+        turnDir = (dir[1],dir[0])
+        oppositeDir = (turnDir[0]*-1,turnDir[1]*-1)
+        for d in [turnDir,oppositeDir]:
+            if seen.get((pos,d),math.inf) < score+1000: continue
+            queue.put((score+1000,pos,d,(pos[0],pos[1],dir[0],dir[1])))
+
+    curr = set()
+    for d in [(1,0),(0,1),(-1,0),(0,-1)]:
+        curr.add(((end[0],end[1]),(d[0],d[1])))
+    visited_squares = set()
+    while len(curr)>0:
+        new_curr = set()
+        for p,d in curr:
+            if p[0] is None:
+                continue
+            if not (p,d) in backtrack: continue
+            new_curr.update(backtrack.get((p,d)))
+            visited_squares.add(p)
+        curr = new_curr
+
+    return len(visited_squares)
 
 def task2(input):
-    pass
+    player = findPlayer(input)
+    end = findEnd(input)
+    return findPathSquares(input,player,end)
 
 def parse(input):
     return [[x for x in line]for line in input]
